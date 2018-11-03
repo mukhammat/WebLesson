f3ef9627a2c56175bf31ed1b67256ad67cd8c5c8
Dosmukhammad <37116036+mukhammat@users.noreply.github.com>
2018-11-03 21:56:31 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/main.css">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap-grid.css">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap-reboot.css">
+
+    </head>
+
+    <body>
+        <header>
+            <div class = "container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="header-top">
+                            <div class="header-name">
+                                <h1>Ресторан BARMALEY</h1>
+                                <h2>Приветствует вас</h2>
+                            </div>
+                            <div class="header-fon">
+                                <ul class="ul-first">
+                                    <li><img src="images/gamburger.png">
+                                        <ul class="ul-two">
+                                            <li><a href="#">Меню</a></li>
+                                            <li><a href="#">О нас</a></li>
+                                            <li><a href="#">Цены</a></li>
+                                            <li><a href="#">Галерея</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="block1">
+                        </div>
+                        <div>
+                            <h1 class="text1_h1">Лучшие повора европы</h1>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="block2">
+                        </div>
+                        <div>
+                            <h1 class="text1_h2">Обслуживание на высшем уровне</h1>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="block3">
+                        </div>
+                        <div>
+                            <h1 class="text1_h3">Уникальные десерты</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="textDJ">
+                            <h1>Лучшие DJ мира выступают <br>у нас</h1>
+                        </div>
+                    </div>
+                    <div class="col-lg-8">
+                        <div class="djimg">
+                        </div>
+                    </div>
+
+                    <div class="listjpg">
+
+                    </div>
+
+                    <div class="col-lg-11">
+                        <p class="djinfo">В нашем ресторане вы забудетее обо всем что вас<br>
+                        беспокоит! Живая музыка и вежливые официанты <br>сделают
+                        этот вечер не забываемым! Повара нашего <br>ресторана
+                        приготовят еду вам таак что вам захочется к нам снова!<br>
+                        Каждую неделю у нас акции и призы для наших<br>
+                        посетителей. Нас выбирают лучшим местам для отдыха</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="alc1">
+                            <a href="#" class="alc1_a1">
+                            <div class="bearimg">
+                            </div>
+                            <div class="beartext">
+                                <p class="ping">Каждый четверг<br> скидки на алкаголь<br> 10%<p>
+                            </div>
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
+
+                    <div class="col-lg-4">
+                        <div class="alc1">
+                            <div class="birth">
+                            </div>
+                            <div class="birthtext">
+                                <p>Встречайте дни<br>рождения вместе с<br>нами</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <div class="alc1">
+                            <div class="mode">
+                            </div>
+                            <div class="modetext">
+                                <p>Все модные вечеринки<br>у нас</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <div class="alc2">
+                            <div class="broneimg">
+                            </div>
+                            <div class="brone">
+                                <p>Бронируйте<br>места заранее</p>
+                            </div>
+                        </div>
+                        <div class="button1">
+                            <p>Забронировать</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <div class="alc2">
+                            <div class="tran">
+                            </div>
+                            <div class="trantext">
+                                <p>Закажите еду<br>на дом</p>
+                            </div>
+                        </div>
+                        <div class="button2">
+                            <p>Заказать</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer>
+            <div class="container-fluit">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="footer1">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+
+
+    </body>
+</html>
